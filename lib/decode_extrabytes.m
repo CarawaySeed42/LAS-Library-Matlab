897d15c74ffd19ad1d1816230d0da32312f4198f
CarawaySeed42 <patrick92.uni@gmx.de>
2022-12-30 01:33:05 +0100

    (Feature Extension) use extrabytes class + change vlr_records to descriptors in extrabytes + look for all descriptors in one VLR + only allow valid var names as properties

--- lib/decode_extrabytes.m
+++ lib/decode_extrabytes.m
@@ -1,41 +1,52 @@
-function extByteStruct = decode_extrabytes(lasStruct)
+function extrabytes = decode_extrabytes(lasStruct)
 %extrabytesStruct = decode_extrabytes(lasStruct)
-%   Decode variable length record (VLR) and extra bytes (extradata) of 
-%   LAS struct into a new struct. 
+%   Decode variable length record (VLR) and extra bytes (extradata) 
+%   following point data of LAS struct to Extrabytes object.
+%   Extra Byte descriptors with the same name are not allowed
+%
+%   Decoding und structuring is done according to specification:
+%   LAS Specification 1.4 R15
 %
 % Input:
 %   lasStruct (struct)          : Structure containing las data with
 %                                 variable length records and extradata
 % Returns:
-%   extByteStruct (struct)      : Struct containing decoded data
+%   extrabytes (Extrabytes)     : Extrabytes object containing decoded data
 %
 %
-%   extByteStruct Fields:
-%   - n fields named by the name of the extra byte specified in it's VLR
+%   Extrabytes class:
+%   - Cell array property called extrabytenames containing extra byte names
+%   - n properties named after the extra byte specified in it's descriptor
 %     With n being the amount of extra data present
-%       - vlr_record: Contents of the decoded VLR
+%       - descriptor:   Contents of the decoded descriptor
 %       - decoded_data: The decoded value of one extradata point with the
-%                       data type specified in VLR. If scaling or offset is 
-%                       set, then decoded_data will be converted to double 
+%                       data type specified in descriptor. If scaling or 
+%                       offset is  set, then decoded_data will be converted
+%                       to double 
 %
 % Example:
-%   There are 4 extra bytes. VLR specifies that both of them are uint16.
-%   VLR states the names "extra1" and "extra2". This results in two fields 
-%   in extByteStruct. Both fields contain their respective decoded VLR in
-%   vlr_record and the decoded uint16 value in decoded_data
+%   There are 4 bytes following point data. Descriptors specifies that
+%   there are two extra data byte values and both of them are uint16.
+%   Descriptors states the names "extra1" and "extra2". This results in 
+%   three properties in extrabytes. Two properties contain their respective 
+%   decoded descriptor field and the decoded uint16 value in decoded_data.
+%   extrabytenames property specifies the names of the extra data values
 %
 % Caution:
 %   It will be assumed that the first extra byte VLR describes the first
 %   extra value, the second describes the second,...
-%   Undocumented extra bytes will be ignored and can not exist between two
-%   described extra values. Undocumented extra bytes have to be decoded by
-%   the user if they know their content.
+%
+%   Undocumented extra bytes will have their specified size extracted but
+%   are assumed to be of type uint64 in any case
+%   Undocumented AND undescribed extra bytes will be ignored and can not 
+%   exist between two described extra values. Undocumented extra bytes in
+%   general have to be decoded by the user if they know their content.
 %
 % Copyright (c) 2022, Patrick Kümmerle
 % Licence: see the included file
 %
 %-------------------------------------------------------------------------
-% The following is how an extrabyte VLR is set up according to
+% The following is how an extrabyte descriptor is set up according to
 % LAS 1.4 Revision 15 specification
 %
 % struct EXTRA_BYTES {
@@ -59,27 +70,9 @@ function extByteStruct = decode_extrabytes(lasStruct)
 
 %% Data Type Lookup Table
 
-% Unused and undocumented types get the size of 0 bytes
-% Layout: [from_num, to_num, matlab_data_type, sizeof_type] 
-datatypeLookup = [...
-    0,  0,  {'uint64'},  0;   ...  %  undocumented extra bytes specify value in options field
-    1,  1,  {'uint8'},   1;   ...  %  unsigned char 1 byte
-    2,  2,  {'char'},    1;   ...  %  char 1 byte
-    3,  3,  {'uint16'},  2;   ...  %  unsigned short 2 bytes
-    4,  4,  {'int16'},   2;   ...  %  short 2 bytes
-    5,  5,  {'uint32'},  4;   ...  %  unsigned long 4 bytes
-    6,  6,  {'int32'},   4;   ...  %  long 4 bytes
-    7,  7,  {'uint64'},  8;   ...  %  unsigned long long 8 bytes
-    8,  8,  {'int64'},   8;   ...  %  long long 8 bytes
-    9,  9,  {'float'},   4;   ...  %  float 4 bytes
-    10, 10, {'double'},  8;   ...  %  double 8 bytes
-    11, 30, {'uint64'},  0;   ...  %  Deprecated deprecated
-    31, 255,{'uint64'},  0;   ...  %  Reserved not assigned
-];
-
-unsignedIndices = [0,1,3,5,7];
-signedIndices   = [2,4,6,8];
-floatingIndices = [9,10];
+% Static information about potential data types of extrabytes
+datatypeLookup = Extrabytes.GetDataTypeLUT();
+datatypeIndices = Extrabytes.GetDataTypeIndices();
 
 %% Input Check and Initializations
 
@@ -93,67 +86,95 @@ if las_extrabyte_count == 0
     error('LAS Struct has no extradata field!')
 end
 
-vlr_documented_bytes_count   = 0;
-vlr_is_extrabyte            = [lasStruct.variablerecords.record_id] == 4;
-vlr_is_extrabyte_index      = find(vlr_is_extrabyte);
+% How many extra byte VLRs exist. There should be only 0 or 1 but if a
+% writer writes a extrabyte VLR per extradata value then this will also be
+% supported
+vlr_documented_bytes_count  = 0;
+vlr_is_extrabyte_index      = find([lasStruct.variablerecords.record_id] == 4);
 vlr_extrabyte_count         = numel(vlr_is_extrabyte_index);
 
 %% Processing
 
-% Unpack extrabyte vlr data and create struct field with extracted name
-extByteStruct = struct();
-field_names_array = cell(vlr_extrabyte_count,1);
+% Unpack extrabyte vlr data and create entry in extrabyte class instance
+extrabytes = Extrabytes();
+descriptor_Size = 192;
 
-for i = 1:vlr_extrabyte_count
+% Iterate through extrabyte VLRs (Should be zero or one)
+for k = 1:vlr_extrabyte_count
     
-    extrabyte_name = char(lasStruct.variablerecords(i).data(5:36))';
-    field_name = strrep(strcat(extrabyte_name), ' ', '_');
-    field_names_array(i) = {field_name};
-    
-    extByteStruct.(field_name).vlr_record.reserved       = uint8(lasStruct.variablerecords(i).data(1:2));             % 2 Bytes
-    extByteStruct.(field_name).vlr_record.data_type.raw   = uint8(lasStruct.variablerecords(i).data(3));              % 1 Byte
-    
-    % Lookup Data Type in table
-    typeTmp         = extByteStruct.(field_name).vlr_record.data_type.raw;
-    typeIndexTmp    = find(typeTmp >= cell2mat(datatypeLookup(:,1)) & typeTmp <= cell2mat(datatypeLookup(:,2)));
-    castTmp         = datatypeLookup{typeIndexTmp,3};
-    dataSizeTmp     = datatypeLookup{typeIndexTmp,4};
-    
-    % No_data, min and max should be upcast to 8 bytes
-    upcastTmp       = 'uint64';
-    if sum(typeTmp == signedIndices) > 0
-        upcastTmp       = 'int64';
-    elseif sum(typeTmp == floatingIndices) > 0
-        upcastTmp       = 'double';
+    current_vlr_index = vlr_is_extrabyte_index(k);
+    extrabyte_descriptor_count = round(length(lasStruct.variablerecords(current_vlr_index).data) / descriptor_Size);
+     if (mod(lasStruct.variablerecords(current_vlr_index).record_length, 192) ~= 0)
+        error('Record Length of Extra Byte Variable Length Record is not a multiple of %d', descriptor_Size);
     end
+    
+    % Extract information from every descriptor
+    for i = 1:extrabyte_descriptor_count
         
-    extByteStruct.(field_name).vlr_record.data_type.matlab_type = castTmp;
-    extByteStruct.(field_name).vlr_record.data_type.size = dataSizeTmp;
-    
-    extByteStruct.(field_name).vlr_record.options.raw = uint8(lasStruct.variablerecords(i).data(4));                            % 1 Byte
-    extByteStruct.(field_name).vlr_record.name        = extrabyte_name;                                                         % 32 Byte
-    extByteStruct.(field_name).vlr_record.unused      = uint8(lasStruct.variablerecords(i).data(37:40));                        % 4 Byte
-    extByteStruct.(field_name).vlr_record.no_data     = typecast(lasStruct.variablerecords(i).data(41:48), upcastTmp);          % 8 Bytes any type
-    extByteStruct.(field_name).vlr_record.deprecated1 = uint8(lasStruct.variablerecords(i).data(49:64));                        % 16 Bytes
-    extByteStruct.(field_name).vlr_record.min         = typecast(lasStruct.variablerecords(i).data(65:72), upcastTmp);          % 8 Bytes any type
-    extByteStruct.(field_name).vlr_record.deprecated2 = uint8(lasStruct.variablerecords(i).data(73:88));                        % 16 Bytes
-    extByteStruct.(field_name).vlr_record.max         = typecast(lasStruct.variablerecords(i).data(89:96), upcastTmp);          % 8 Bytes any type
-    extByteStruct.(field_name).vlr_record.deprecated3 = uint8(lasStruct.variablerecords(i).data(97:112));                       % 16 Bytes
-    extByteStruct.(field_name).vlr_record.scale       = typecast(uint8(lasStruct.variablerecords(i).data(113:120)), 'double');  % 8 Bytes
-    extByteStruct.(field_name).vlr_record.deprecated4 = uint8(lasStruct.variablerecords(i).data(121:136));                      % 16 Bytes
-    extByteStruct.(field_name).vlr_record.offset      = typecast(uint8(lasStruct.variablerecords(i).data(137:144)), 'double');  % 8 Bytes
-    extByteStruct.(field_name).vlr_record.deprecated5 = uint8(lasStruct.variablerecords(i).data(145:160));                      % 16 Bytes
-    extByteStruct.(field_name).vlr_record.description = char(lasStruct.variablerecords(i).data(161:192))';                      % 32 Bytes
-    
-    % Decode options
-    extByteStruct.(field_name).vlr_record.options.no_data_bit = bitand(extByteStruct.(field_name).vlr_record.options.raw, 1);
-    extByteStruct.(field_name).vlr_record.options.min_bit     = bitand(bitshift(extByteStruct.(field_name).vlr_record.options.raw, -1, 'uint8'), 1);
-    extByteStruct.(field_name).vlr_record.options.max_bit     = bitand(bitshift(extByteStruct.(field_name).vlr_record.options.raw, -2, 'uint8'), 1);
-    extByteStruct.(field_name).vlr_record.options.scale_bit   = bitand(bitshift(extByteStruct.(field_name).vlr_record.options.raw, -3, 'uint8'), 1);
-    extByteStruct.(field_name).vlr_record.options.offset_bit  = bitand(bitshift(extByteStruct.(field_name).vlr_record.options.raw, -4, 'uint8'), 1);
-    
-    % Sum the number of extrabytes
-    vlr_documented_bytes_count = vlr_documented_bytes_count + extByteStruct.(field_name).vlr_record.data_type.size;
+        descriptor_data = lasStruct.variablerecords(current_vlr_index).data(((i-1)*descriptor_Size)+1:i*descriptor_Size);
+        
+        % Add property with name of extra value, but make sure that the
+        % name is unique and valid
+        extrabyte_name = char(descriptor_data(5:36))';
+        field_name = VariableNames.MakeValid(extrabyte_name);
+        nameOccupied = strcmp(extrabytes.extrabytenames, field_name);
+        if any(nameOccupied)
+            field_name = strcat(field_name,'_', num2str(sum(nameOccupied)+1));
+        end
+        
+        extrabytes.AddExtrabytes({field_name});
+        
+        extrabytes.(field_name).descriptor.reserved       = uint8(descriptor_data(1:2));            % 2 Bytes
+        extrabytes.(field_name).descriptor.data_type.raw  = uint8(descriptor_data(3));              % 1 Byte
+        extrabytes.(field_name).descriptor.options.raw    = uint8(descriptor_data(4));              % 1 Byte
+        
+        % Lookup Data Type in table
+        typeTmp         = extrabytes.(field_name).descriptor.data_type.raw;
+        typeIndexTmp    = find(typeTmp >= cell2mat(datatypeLookup(:,1)) & typeTmp <= cell2mat(datatypeLookup(:,2)));
+        matlabType      = datatypeLookup{typeIndexTmp,3};
+        dataSize        = datatypeLookup{typeIndexTmp,4};
+        
+        % If data type is zero then the options field specifies how many
+        % undocumented bytes there are
+        if (typeTmp == 0)
+            dataSize = extrabytes.(field_name).descriptor.options.raw;
+        end
+        
+        % No_data, min and max should be upcast to 8 bytes
+        upcastTmp       = 'uint64';
+        if sum(typeTmp == datatypeIndices.signed) > 0
+            upcastTmp       = 'int64';
+        elseif sum(typeTmp == datatypeIndices.float) > 0
+            upcastTmp       = 'double';
+        end
+        
+        extrabytes.(field_name).descriptor.data_type.matlab_type = matlabType;
+        extrabytes.(field_name).descriptor.data_type.size = dataSize;
+        
+        extrabytes.(field_name).descriptor.name        = extrabyte_name;                                       % 32 Byte
+        extrabytes.(field_name).descriptor.unused      = uint8(descriptor_data(37:40));                        % 4 Byte
+        extrabytes.(field_name).descriptor.no_data     = typecast(descriptor_data(41:48), upcastTmp);          % 8 Bytes any type
+        extrabytes.(field_name).descriptor.deprecated1 = uint8(descriptor_data(49:64));                        % 16 Bytes
+        extrabytes.(field_name).descriptor.min         = typecast(descriptor_data(65:72), upcastTmp);          % 8 Bytes any type
+        extrabytes.(field_name).descriptor.deprecated2 = uint8(descriptor_data(73:88));                        % 16 Bytes
+        extrabytes.(field_name).descriptor.max         = typecast(descriptor_data(89:96), upcastTmp);          % 8 Bytes any type
+        extrabytes.(field_name).descriptor.deprecated3 = uint8(descriptor_data(97:112));                       % 16 Bytes
+        extrabytes.(field_name).descriptor.scale       = typecast(uint8(descriptor_data(113:120)), 'double');  % 8 Bytes
+        extrabytes.(field_name).descriptor.deprecated4 = uint8(descriptor_data(121:136));                      % 16 Bytes
+        extrabytes.(field_name).descriptor.offset      = typecast(uint8(descriptor_data(137:144)), 'double');  % 8 Bytes
+        extrabytes.(field_name).descriptor.deprecated5 = uint8(descriptor_data(145:160));                      % 16 Bytes
+        extrabytes.(field_name).descriptor.description = char(descriptor_data(161:192))';                      % 32 Bytes
+        
+        % Decode options
+        extrabytes.(field_name).descriptor.options.no_data_bit = bitand(extrabytes.(field_name).descriptor.options.raw, 1);
+        extrabytes.(field_name).descriptor.options.min_bit     = bitand(bitshift(extrabytes.(field_name).descriptor.options.raw, -1, 'uint8'), 1);
+        extrabytes.(field_name).descriptor.options.max_bit     = bitand(bitshift(extrabytes.(field_name).descriptor.options.raw, -2, 'uint8'), 1);
+        extrabytes.(field_name).descriptor.options.scale_bit   = bitand(bitshift(extrabytes.(field_name).descriptor.options.raw, -3, 'uint8'), 1);
+        extrabytes.(field_name).descriptor.options.offset_bit  = bitand(bitshift(extrabytes.(field_name).descriptor.options.raw, -4, 'uint8'), 1);
+        
+        % Sum the number of extrabytes
+        vlr_documented_bytes_count = vlr_documented_bytes_count + extrabytes.(field_name).descriptor.data_type.size;
+    end
 end
 
 if vlr_documented_bytes_count > las_extrabyte_count
@@ -161,27 +182,39 @@ if vlr_documented_bytes_count > las_extrabyte_count
 end
 
 if vlr_documented_bytes_count < las_extrabyte_count
-    warning('There are %d undocumented and undescribed extra bytes! Those will be ignored!', las_extrabyte_count-vlr_documented_bytes_count);
+    warning('There are %d undocumented and undescribed or unsupported extra bytes! Those will be ignored!', las_extrabyte_count-vlr_documented_bytes_count);
 end
 
 % Now finally decode the extrabytes at the end of every single data point
 byte_start = 1;
-for i = 1:vlr_extrabyte_count
+for i = 1:length(extrabytes.extrabytenames)
     
-    currentField = extByteStruct.(field_names_array{i});
-    byte_count = currentField.vlr_record.data_type.size;
+    name = extrabytes.extrabytenames{i};
+    byte_count = extrabytes.(name).descriptor.data_type.size;
     
+    % If undocumented or unsupported extra bytes then do not decode
+    if extrabytes.(name).descriptor.options.raw == 0
+        extrabytes.(name).decoded_data = [];
+        byte_start = byte_start + byte_count;
+        continue;
+    elseif byte_count == 0
+        extrabytes.(name).decoded_data = [];
+        warning(['Extrabyte type according to LAS 1.4 R15 not supported! ',...
+            'Extra data after ''%s'' will desync!'],  name);
+        continue;
+    end
+        
     % Create extrabyte data with correct data type
-    extByteStruct.(field_names_array{i}).decoded_data = zeros(size(lasStruct.extradata, 1),1);
+    extrabytes.(name).decoded_data = zeros(size(lasStruct.extradata, 1),1);
     tmpVector = reshape(lasStruct.extradata(:,byte_start:byte_start+byte_count-1)', 1, [])';
-    extByteStruct.(field_names_array{i}).decoded_data = typecast(tmpVector, currentField.vlr_record.data_type.matlab_type);
+    extrabytes.(name).decoded_data = typecast(tmpVector, extrabytes.(name).descriptor.data_type.matlab_type);
     
-    if currentField.vlr_record.options.scale_bit
-        extByteStruct.(field_names_array{i}).decoded_data = double(extByteStruct.(field_names_array{i}).decoded_data) * extByteStruct.(field_name).vlr_record.scale;
+    if extrabytes.(name).descriptor.options.scale_bit
+        extrabytes.(name).decoded_data = double(extrabytes.(name).decoded_data) * extrabytes.(name).descriptor.scale;
     end
     
-    if currentField.vlr_record.options.offset_bit
-        extByteStruct.(field_names_array{i}).decoded_data = double(extByteStruct.(field_names_array{i}).decoded_data) + extByteStruct.(field_name).vlr_record.offset;
+    if extrabytes.(name).descriptor.options.offset_bit
+        extrabytes.(name).decoded_data = double(extrabytes.(name).decoded_data) + extrabytes.(name).descriptor.offset;
     end
     
     % Go to next column
